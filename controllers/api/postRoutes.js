a70715c9d473a1221f6067e899cea54773b14557
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-09-19 22:48:28 -0500

    Completed post routes, will test functionality after completing all routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const withAuth = require('../../utils/auth');
+
+// Post route to create a post
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            title: req.body.title,
+            contents: req.body.contents,
+            user_id: req.session.user_id,
+
+    });
+
+    res.status(200).json({ message: 'Post created successfully!' });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Put route to update a post
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatePost = await Post.update({
+            title: req.body.title,
+            contents: req.body.contents,
+        }, 
+        {
+            where: {
+                id: req.params.id
+            }
+        });
+        
+        if (!updatePost) {
+            res.status(400).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        res.json(updatePost);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// Route to delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const deletePost = await Post.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        if (!deletePost) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        res.json(deletePost);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
